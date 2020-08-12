66f50946e0b6789508dd0d46f9e9493db527285a
Atomy <tl2f@outlook.com>
2020-08-12 08:23:52 +0800

    add ref property and complement press Enter to add item

--- react-demo01-singlePage/src/Todo.js
+++ react-demo01-singlePage/src/Todo.js
@@ -11,6 +11,7 @@ class Todo extends Component {
         this.InputChange = this.InputChange.bind(this)
         this.AddItem = this.AddItem.bind(this)
         this.DeleteItem = this.DeleteItem.bind(this)
+        this.keyDown=this.keyDown.bind(this)
     }
     render() { 
         return ( 
@@ -18,9 +19,12 @@ class Todo extends Component {
                 <input 
                     placeholder={this.state.placeholder}
                     value={this.state.inputValue}
-                    onChange={this.InputChange} />
+                    onChange={this.InputChange} 
+                    ref={inputItem=>this._inputItem=inputItem}
+                    />
                 <button
                     onClick={this.AddItem}
+                    ref={btn=>this._btn=btn}
                 >Add</button>
 
                 <ul>
@@ -41,8 +45,11 @@ class Todo extends Component {
          );
     }
 
+    componentDidMount(){
+        document.addEventListener('keydown',this.keyDown)
+    }
+
     InputChange(e){
-        console.log(e.target.value)
         this.setState({inputValue:e.target.value})
     }
 
@@ -59,6 +66,12 @@ class Todo extends Component {
         this.setState({
             listData:newListData
         })
+    }
+
+    keyDown(e){
+        if(e.keyCode===13&&document.activeElement===this._inputItem){
+            this._inputItem.value?this.AddItem():alert("input something")
+        }
     }
 }
 
